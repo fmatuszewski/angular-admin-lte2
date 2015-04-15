69951f373f9fb5c427a78da97daf9fb940147791
Filip Matuszewski <filip.matuszewski@gmail.com>
2015-04-15 09:34:31 +0200

    charts

--- dist/js/app.js
+++ dist/js/app.js
@@ -56,9 +56,9 @@ adminLTE.config(['$urlRouterProvider','$stateProvider',function($urlRouterProvid
 adminLTE.directive('sidebar', function(){
 	return {
 		restrict: 'C',
-		link: function(scope, element, attrs){
+		compile: function(tElement, tAttrs, transclude){
 			  //Enable sidebar tree view controls
-				$.AdminLTE.tree(element);
+				$.AdminLTE.tree(tElement);
 		}
 	};
 });
@@ -66,14 +66,88 @@ adminLTE.directive('header',function(){
 	return {
 		restrict: 'E',
 		templateUrl: 'templates/header.html',
-		link: function(scope, element, attrs){
-			$.AdminLTE.pushMenu($(element).find('.sidebar-toggle'));
+		compile: function(tElement, tAttrs, transclude){
+			$.AdminLTE.pushMenu($(tElement).find('.sidebar-toggle'));
 		}
 	};
 });
 
-  
-  
+adminLTE.directive('knob',function(){
+	return {
+		restrict: 'C',
+		compile: function(tElement, tAttrs, transclude){
+			tElement.knob({
+          /*change : function (value) {
+           //console.log("change : " + value);
+           },
+           release : function (value) {
+           console.log("release : " + value);
+           },
+           cancel : function () {
+           console.log("cancel : " + this.value);
+           },*/
+          draw: function () {
+
+            // "tron" case
+            if (this.$.data('skin') == 'tron') {
+
+              var a = this.angle(this.cv)  // Angle
+                      , sa = this.startAngle          // Previous start angle
+                      , sat = this.startAngle         // Start angle
+                      , ea                            // Previous end angle
+                      , eat = sat + a                 // End angle
+                      , r = true;
+
+              this.g.lineWidth = this.lineWidth;
+
+              this.o.cursor
+                      && (sat = eat - 0.3)
+                      && (eat = eat + 0.3);
+
+              if (this.o.displayPrevious) {
+                ea = this.startAngle + this.angle(this.value);
+                this.o.cursor
+                        && (sa = ea - 0.3)
+                        && (ea = ea + 0.3);
+                this.g.beginPath();
+                this.g.strokeStyle = this.previousColor;
+                this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sa, ea, false);
+                this.g.stroke();
+              }
+
+              this.g.beginPath();
+              this.g.strokeStyle = r ? this.o.fgColor : this.fgColor;
+              this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sat, eat, false);
+              this.g.stroke();
+
+              this.g.lineWidth = 2;
+              this.g.beginPath();
+              this.g.strokeStyle = this.o.fgColor;
+              this.g.arc(this.xy, this.xy, this.radius - this.lineWidth + 1 + this.lineWidth * 2 / 3, 0, 2 * Math.PI, false);
+              this.g.stroke();
+
+              return false;
+            }
+          }
+        });
+		        /* END JQUERY KNOB */
+		}
+	}
+});
+
+adminLTE.directive('sparkline',function(){
+	return {
+		restrict: 'C',
+		compile: function(tElement, tAttrs, transclude){
+			tElement.each(function () {
+			  var $this = $(this);
+			  $this.sparkline('html', $this.data());
+			});
+		}
+	}
+});
+ 
+
 /* AdminLTE
  *
  * @type Object
